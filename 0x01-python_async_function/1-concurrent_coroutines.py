56acd966d09fe1c2d0aca674903a293907de163d
Moussaddak <meddeb.moussaddak@gmail.com>
2021-07-21 03:55:41 +0100

    up

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -7,10 +7,10 @@ from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, maw_delay: int) -> List[float]:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """
     Tasks
     """
     # taskList = [wait_random(maw_delay) for _ in range(n)]
-    taskList = [asyncio.create_task(wait_random(maw_delay)) for _ in range(n)]
+    taskList = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
     return [await task for task in asyncio.as_completed(taskList)]
--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -6,9 +6,9 @@ from typing import List
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def task_wait_n(n: int, maw_delay: int) -> List[float]:
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """
     Tasks
     """
-    taskList = [task_wait_random(maw_delay) for _ in range(n)]
+    taskList = [task_wait_random(max_delay) for _ in range(n)]
     return [await task for task in asyncio.as_completed(taskList)]
