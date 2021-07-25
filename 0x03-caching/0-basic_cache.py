aea3afcad5e14ae4e49ddccf59c1ae35fdce9fdf
Moussaddak <meddeb.moussaddak@gmail.com>
2021-07-25 02:05:09 +0100

    up

--- 0x03-caching/0-basic_cache.py
+++ 0x03-caching/0-basic_cache.py
@@ -12,7 +12,7 @@ class BasicCache(BaseCaching):
     def put(self, key, item):
         """ Add an item in the cache
         """
-        if key == None or item == None:
+        if key is None or item is None:
             return
         self.cache_data[key] = item
 
