6b069a5769be9b910e25b4917bb504ff1125b000
Moussaddak <meddeb.moussaddak@gmail.com>
2021-07-25 04:09:23 +0100

    up

---
+++ 0x03-caching/3-lru_cache.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+""""3. LRU Caching
+"""
+
+from base_caching import BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """ LRUCache defines:
+      - caching system
+    """
+
+    def __init__(self):
+        """ Initiliaze
+        """
+        super().__init__()
+
+    def put(self, key, item):
+        """ Add an item in the cache
+        """
+        itemKeys = self.cache_data.keys()
+        numberOfItem = len(itemKeys)
+        if key and item:
+            if key in itemKeys:
+                self.cache_data.pop(key)
+            if numberOfItem >= self.MAX_ITEMS and key not in itemKeys:
+                ListKeys = list(itemKeys)
+                firstItem = ListKeys[0]
+                self.cache_data.pop(firstItem)
+                print("DISCARD: {}".format(firstItem))
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """ Get an item by key
+        """
+        if key is None:
+            return None
+        return self.cache_data.get(key, None)
