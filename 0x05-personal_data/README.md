29c3ab0d1287570a432b60786bf0097d32c34d6d
Moussaddak <meddeb.moussaddak@gmail.com>
2021-08-02 19:22:51 +0100

    up

---
+++ 0x05-personal_data/README.md
@@ -0,0 +1,9 @@
+# 0x05. Personal data
+
+* Examples of Personally Identifiable Information (PII)
+
+* How to implement a log filter that will obfuscate PII fields
+
+* How to encrypt a password and check the validity of an input password
+
+* How to authenticate to a database using environment variables
---
+++ 0x05-personal_data/filtered_logger.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+"""0. Regex-ing"""
+from typing import List
+import re
+import logging
+
+PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
+
+
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
+    """
+    :param fields:
+    :param redaction:
+    :param message:
+    :param separator:
+    :return: log message obfuscated
+    """
+    for field in fields:
+        message = re.sub(f'{field}=.*?{separator}',
+                         f"{field}={redaction}{separator}",
+                         message)
+    return message
+
+
+def get_logger() -> logging.Logger:
+    """
+    :return: function that takes no arguments and returns a logging.Logger object
+    """
+    logger = logging.getLogger("user_data")  # logger name is "user_data"
+    logger.propagate = False  # not propagate messages to other loggers
+    logger.setLevel(logging.INFO)  # handle only INFO level
+    formatter = RedactingFormatter(PII_FIELDS)
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(formatter)
+    logger.addHandler(stream_handler)
+    return logger
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """
+        filter values in incoming log records using filter_datum
+        :param record:
+        :return:
+        """
+        return filter_datum(self.fields, self.REDACTION,
+                            logging.Formatter(self.FORMAT).format(record),
+                            self.SEPARATOR)
