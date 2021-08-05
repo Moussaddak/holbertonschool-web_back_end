1642d22738a98e425ee3892d5eea67b3e9f514e3
Moussaddak <meddeb.moussaddak@gmail.com>
2021-08-05 03:00:04 +0100

    up

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -9,6 +9,9 @@ from api.v1.auth.auth import Auth
 
 
 class BasicAuth(Auth):
+    """
+    BasicAuth class
+    """
 
     def extract_base64_authorization_header(
             self, authorization_header: str) -> str:
