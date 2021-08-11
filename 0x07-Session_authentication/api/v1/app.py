845431a29f52e6487d87c4967c097ce3ba56c5c2
Moussaddak <meddeb.moussaddak@gmail.com>
2021-08-11 17:27:50 +0100

    up

--- 0x07-Session_authentication/api/v1/app.py
+++ 0x07-Session_authentication/api/v1/app.py
@@ -37,7 +37,7 @@ def before_request() -> str:
     if auth is None:
         return None
     if not auth.require_auth(request.path, excluded_paths) \
-            and auth.authorization_header(request):
+            and not auth.authorization_header(request):
         return abort(401)
     if not auth.require_auth(request.path, excluded_paths):
         return None
