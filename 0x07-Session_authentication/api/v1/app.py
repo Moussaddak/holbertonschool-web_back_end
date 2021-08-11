60da347f16b7ab64a29aa13f2649276507b210ff
Moussaddak <meddeb.moussaddak@gmail.com>
2021-08-11 17:38:09 +0100

    up

--- 0x07-Session_authentication/api/v1/app.py
+++ 0x07-Session_authentication/api/v1/app.py
@@ -38,9 +38,8 @@ def before_request() -> str:
         return None
     if not auth.require_auth(request.path, excluded_paths):
         return None
-    if (auth.session_cookie(request) is None
-            and auth.authorization_header(request) is None)\
-            or auth.authorization_header(request) is None:
+    if auth.session_cookie(request) is None \
+            and auth.authorization_header(request) is None:
         return abort(401)
     if auth.current_user(request) is None:
         return abort(403)
