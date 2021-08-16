1f1b7440253708897ba1656ec35aec36f9a25f06
Moussaddak <meddeb.moussaddak@gmail.com>
2021-08-16 21:32:29 +0100

    up

---
+++ 0x08-user_authentication_service/db.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+"""DB module
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
+from user import Base, User
+
+
+class DB:
+    """DB class
+    """
+
+    def __init__(self) -> None:
+        """Initialize a new DB instance
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=False)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self) -> Session:
+        """Memoized session object
+        """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email: str, hashed_password: str) -> User:
+        """
+            add user
+        :param email:
+        :param hashed_password:
+        :return:
+        """
+        user = User(email=email, hashed_password=hashed_password)
+        self._session.add(user)
+        self._session.commit()
+        return user
+
+    def find_user_by(self, **kwargs) -> User:
+        """
+            find user
+        :param kwargs:
+        :return:
+        """
+        try:
+            key, value = list(kwargs.items())[0][0], \
+                         list(kwargs.items())[0][1]
+            user = self._session.query(User).filter(
+                getattr(User, key) == value).first()
+        except InvalidRequestError:
+            raise InvalidRequestError
+        except AttributeError:
+            raise NoResultFound
+
+        if user is None:
+            raise NoResultFound
+        return user
+
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """
+            update user
+        :param user_id:
+        :param kwargs:
+        :return:
+        """
+        user = self.find_user_by(id=user_id)
+        for key, value in kwargs.items():
+            if not hasattr(user, key):
+                raise ValueError
+            else:
+                setattr(user, key, value)
+        self._session.commit()
