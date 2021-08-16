ab3e8861678a8168a1c45826cf69147536a9f63e
Moussaddak <meddeb.moussaddak@gmail.com>
2021-08-16 21:42:43 +0100

    up

--- 0x07-Session_authentication/api/v1/app.py
+++ 0x07-Session_authentication/api/v1/app.py
@@ -32,6 +32,11 @@ elif getenv("AUTH_TYPE") == "session_exp_auth":
 
     auth = SessionExpAuth()
 
+elif getenv("AUTH_TYPE") == "session_db_auth":
+    from api.v1.auth.session_db_auth import SessionDBAuth
+
+    auth = SessionDBAuth()
+
 
 @app.before_request
 def before_request() -> str:
