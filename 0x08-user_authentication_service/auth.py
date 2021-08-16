3e93f6397be556042ece79597ce108a44ba01141
Moussaddak <meddeb.moussaddak@gmail.com>
2021-08-16 22:12:26 +0100

    up

---
+++ 0x08-user_authentication_service/auth.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+"""
+Module of Auth
+"""
+
+import bcrypt
+from sqlalchemy.exc import NoResultFound
+
+from db import DB
+from user import User
+
+
+def _hash_password(password: str) -> bytes:
+    """
+        Hash password
+    :param password:
+    :return:
+    """
+    salt = bcrypt.gensalt()
+    hashed = bcrypt.hashpw(password.encode('utf-8'), salt)
+    return hashed
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """
+
+        :param email:
+        :param password:
+        :return:
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            if user:
+                raise ValueError("User {} already exists".format(email))
+        except NoResultFound:
+            pass
+        hashed = _hash_password(password)
+        return self._db.add_user(email, hashed)
--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -31,7 +31,7 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email: str, hashed_password: str) -> User:
+    def add_user(self, email: str, hashed_password: bytes) -> User:
         """
             add user
         :param email:
