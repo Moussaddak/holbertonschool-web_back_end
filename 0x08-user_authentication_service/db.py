e9fff1273ce0f0f8b42f0e17ea54283fe9a53c47
Moussaddak <meddeb.moussaddak@gmail.com>
2021-08-16 21:45:03 +0100

    up

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -63,7 +63,7 @@ class DB:
             raise NoResultFound
         return user
 
-    def update_user(self, user_id: int, **kwargs) -> None:
+    def update_user(self, user_id: int, **kwargs):
         """
             update user
         :param user_id:
