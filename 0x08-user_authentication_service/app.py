0b15399a93e8a8ff9fbf5b6666dc5c5eb9800687
Moussaddak <meddeb.moussaddak@gmail.com>
2021-08-17 05:12:13 +0100

    up

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -106,5 +106,25 @@ def get_reset_password_token():
     return jsonify({"email": email, "reset_token": token}), 200
 
 
+@app.route('/reset_password', methods=['PUT'])
+def update_password():
+    """
+        Update password end-point
+    :return:
+    """
+    email = request.form.get('email')
+    token = request.form.get('reset_token')
+    new_password = request.form.get('new_password')
+
+    if email is None or token is None \
+            or new_password is None:
+        abort(403)
+    try:
+        AUTH.update_password(token, new_password)
+        return jsonify({"email": email, "message": "Password updated"})
+    except ValueError:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
