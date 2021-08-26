51a603514de578da4011a69221519c0ed2348bdd
Moussaddak <meddeb.moussaddak@gmail.com>
2021-08-26 20:54:36 +0100

    up

--- 0x0A-i18n/0-app.py
+++ 0x0A-i18n/0-app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def Hello():
+def Hello() -> str:
     """
          route and an index.html
     :return:
---
+++ 0x0A-i18n/5-app.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+""" Basic Flask App """
+from flask import Flask, render_template, request, g
+from flask_babel import Babel, _
+
+
+class Config(object):
+    """ Config Class """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+def get_user(user_id: int) -> dict:
+    """
+        get user by id
+    :param user_id:
+    :return:
+    """
+    if user_id and type(user_id) in [int, str]:
+        return users.get(int(user_id))
+
+
+@babel.localeselector
+def get_locale():
+    """ Get locale from request """
+    if request.args.get('locale') == 'fr':
+        return 'fr'
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.before_request
+def before_request():
+    """
+        find a user
+    :return:
+    """
+    user_id = request.args.get('login_as')
+    g.user = get_user(user_id)
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def Hello():
+    """
+        route and an index.html
+    :return:
+    """
+    if g.user:
+        # print(g.user['name'])
+        msg = _('logged_in_as', username=g.user['name'])
+    else:
+        msg = _('not_logged_in')
+    # print(msg)
+    return render_template('5-index.html', title=_('home_title'),
+                           header=_('home_header'), login=msg)
+
+
+if __name__ == '__main__':
+    app.run()
