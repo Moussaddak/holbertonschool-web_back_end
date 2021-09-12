24af36412c340e9233b951489b0e3306a830cf2c
Moussaddak <meddeb.moussaddak@gmail.com>
2021-09-12 23:59:36 +0100

    up

--- 0x0E-ES6_basic/3-default-parameter.js
+++ 0x0E-ES6_basic/3-default-parameter.js
@@ -1,4 +1,4 @@
-export default function getSumOfHoods(initialNumber, expansion1989 = 89, expansion2019 = 19) {
+export default function getSumOfHoods(initialNumber = 0, expansion1989 = 89, expansion2019 = 19) {
   // if (expansion1989 === undefined) {
   //     expansion1989 = 89;
   // }
