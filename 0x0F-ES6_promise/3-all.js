72868e06b9f25e2a6b19ff992ec9d168ae2bb9c9
Moussaddak <meddeb.moussaddak@gmail.com>
2021-09-14 22:14:43 +0100

    up

--- 0x0F-ES6_promise/3-all.js
+++ 0x0F-ES6_promise/3-all.js
@@ -3,7 +3,6 @@ import { uploadPhoto, createUser } from './utils';
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
     .then((values) => {
-      console.log(values);
       console.log(
         values[0].body, values[1].firstName, values[1].lastName,
       );
