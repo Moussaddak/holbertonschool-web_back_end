b9804d2db3fdf671c21d96036888eb6c9a00e49c
Moussaddak <meddeb.moussaddak@gmail.com>
2021-09-19 15:56:03 +0100

    up

---
+++ 0x10-ES6_classes/3-currency.js
@@ -0,0 +1,38 @@
+export default class Currency {
+  constructor(code, name) {
+    if (!(typeof code == 'string'))  {
+      throw new TypeError('Currency must be a string');
+    }
+    if (!(typeof name == 'string'))  {
+      throw new TypeError('Name must be a string');
+    }
+    this._code = code;
+    this._name = name;
+  }
+
+  set code(code) {
+    if (!(typeof code == 'string')) {
+      throw new TypeError('Currency must be a string');
+    }
+    this._code = code;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set name(name) {
+    if (!(typeof name == 'string')) {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = name;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
