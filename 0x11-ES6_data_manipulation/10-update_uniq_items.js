027182a393158f5d3923a3f919a53b41071c3760
Moussaddak <meddeb.moussaddak@gmail.com>
2021-09-21 23:44:05 +0100

    up

---
+++ 0x11-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,9 @@
+export default function updateUniqueItems(f) {
+    if (!(f instanceof Map)) {
+        throw Error('Cannot process');
+    }
+    f.forEach((value, key) => {
+        value === 1 ? f.set(key, 100) : f.set(key, value)
+    });
+    return f;
+}
