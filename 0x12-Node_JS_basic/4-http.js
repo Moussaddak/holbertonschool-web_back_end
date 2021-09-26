436170d2211e30cb32a5d0e5a58e0c1c3cc9ac13
Moussaddak <meddeb.moussaddak@gmail.com>
2021-09-26 23:19:41 +0100

    up

---
+++ 0x12-Node_JS_basic/4-http.js
@@ -0,0 +1,9 @@
+const http = require('http');
+
+const app = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello Holberton School!');
+}).listen(1245);
+
+module.exports = app;
