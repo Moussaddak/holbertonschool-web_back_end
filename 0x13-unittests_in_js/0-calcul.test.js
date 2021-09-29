4d259b4d7319df8b62e36c40a15b9a32bc30d11b
Moussaddak <meddeb.moussaddak@gmail.com>
2021-09-29 21:42:12 +0100

    up

--- 0x13-unittests_in_js/0-calcul.test.js
+++ 0x13-unittests_in_js/0-calcul.test.js
@@ -3,15 +3,14 @@ const calculateNumber = require("./0-calcul");
 const assert = require('assert');
 
 describe('calculateNumber', () => {
-        it("test package", () => {
-                assert.strictEqual(calculateNumber(1, 1.5), 3);
-                assert.strictEqual(calculateNumber(1.7, 1.5), 4);
-                assert.strictEqual(calculateNumber('text', 1), NaN);
-                assert.strictEqual(calculateNumber('text', 'str'), NaN);
-                assert.strictEqual(calculateNumber(4, 4.8), 9);
-                assert.ok(isNaN(calculateNumber(1, "str")));
-                assert.ok(isNaN(calculateNumber("str", 1,2)));
-                assert.ok(isNaN(calculateNumber("str", "str")));
-        })
-
+    it("test package", () => {
+        assert.strictEqual(calculateNumber(1, 1.5), 3);
+        assert.strictEqual(calculateNumber(1.7, 1.5), 4);
+        assert.strictEqual(calculateNumber('text', 1), NaN);
+        assert.strictEqual(calculateNumber('text', 'str'), NaN);
+        assert.strictEqual(calculateNumber(4, 4.8), 9);
+        assert.ok(isNaN(calculateNumber(1, "str")));
+        assert.ok(isNaN(calculateNumber("str", 1, 2)));
+        assert.ok(isNaN(calculateNumber("str", "str")));
+    })
 });
