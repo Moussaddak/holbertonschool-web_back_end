cbdecf6abf19c3144228f3aefc070b29e4617023
Moussaddak <meddeb.moussaddak@gmail.com>
2021-10-05 22:02:28 +0100

    up

---
+++ 0x14-queuing_system_in_js/2-redis_op_async.js
@@ -0,0 +1,20 @@
+import redis from 'redis';
+const { promisify } = require('util');
+
+
+const client = redis.createClient();
+client.on('connect', (error) => {
+    console.log('Redis client connected to the server');
+});
+client.on('error', (error) => {
+    console.error('Redis client not connected to the server: ERROR_MESSAGE');
+});
+
+client.get = promisify(client.get).bind(client);
+
+const setNewSchool = (schoolName, value) => client.set(schoolName, value, redis.print);
+const displaySchoolValue = async (schoolName) => console.log(`${await client.get(schoolName)}`);
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
---
+++ 0x14-queuing_system_in_js/4-redis_advanced_op.js
@@ -0,0 +1,18 @@
+import redis from 'redis';
+const { promisify } = require('util');
+
+const client = redis.createClient();
+client.on('connect', (error) => {
+    console.log('Redis client connected to the server');
+});
+client.on('error', (error) => {
+    console.error(`Redis client not connected to the server: ${error}`);
+});
+
+client.hset('HolbertonSchools', 'Portland', 50, redis.print);
+client.hset('HolbertonSchools', 'Seattle', 80, redis.print);
+client.hset('HolbertonSchools', 'New York', 20, redis.print);
+client.hset('HolbertonSchools', 'Bogota', 20, redis.print);
+client.hset('HolbertonSchools', 'Cali', 40, redis.print);
+client.hset('HolbertonSchools', 'Paris', 2, redis.print);
+client.hgetall('HolbertonSchools', (k, v) => console.log(v));
